a7de1d8ef4ff1d4694563f8fa1bbeeee8f145731
Hassan Liaqat <hassanliaqat194@gmail.com>
2026-07-22 18:08:13 +0500

    Add new methods to PortScanner class

--- include/PortScanner.h
+++ include/PortScanner.h
@@ -9,6 +9,14 @@ class PortScanner
 {
 public:
     bool initialize();
+    bool createSocket();
+    bool scanPort(const string& ip, int port);
+    void scanRange(const string& ip, int startPort, int endPort);
+    void cleanup();
+
+    string getServiceName(int port);
+    void threadedScan(const string& ip, int port);
+    string resolveHostname(string hostname);
 };
 
 #endif
